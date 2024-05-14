71d5aae10381a70a9d6a1e182ca2884665611372
Obadias Pelembe <Obadias.Pelembe@vm.co.mz>
2024-05-14 20:18:31 +0200

    chore: add infrastructure script

--- infrastructure.sh
+++ infrastructure.sh
@@ -2,4 +2,10 @@
 
 terraform -chdir="infrastructure" init
 
-terraform -chdir="infrastructure" plan
+terraform -chdir="infrastructure" plan -var-file="vars.tfvars"
+
+terraform -chdir="infrastructure" apply -var-file="vars.tfvars"
+
+terraform -chdir="infrastructure" output -raw server_host >> .server_host
+
+echo "linode_server ansible_host=$(eval cat .server_host)" >> hosts
