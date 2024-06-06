4ffd348d33724b2d25b3a71828991686d1200b5a
GreenhatMZ <jr.pelembeobadias@gmail.com>
2024-06-06 19:14:03 +0200

    feat: improve best practices for terraform

--- infrastructure/resource_linode_instance.tf
+++ infrastructure/resource_linode_instance.tf
@@ -1,9 +1,14 @@
+resource "random_password" "linode_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
 resource "linode_instance" "web" {
-  label           = "my-server"
-  image           = "linode/ubuntu18.04"
-  region          = "us-central"
-  type            = "g6-standard-1"
-  authorized_keys = [var.linode_public_keys]
-  root_pass       = var.root_pass
-  tags  = ["linode-server"]
+  label           = local.instance_name
+  image           = local.instance_image
+  region          = local.instance_region
+  type            = local.instance_type
+  root_pass       = random_password.linode_password.result
+  tags  = ["linux-server", local.instance_name]
 }
--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -2,13 +2,3 @@ variable "linode_token" {
   type = string
   description = "Linode API token"
 }
-
-variable "linode_public_keys" {
-  type = string
-  description = "Allowed SSH Keys to access linode instance"
-}
-
-variable "root_pass" {
-  type = string
-  description = "Linode instance root password"
-}
